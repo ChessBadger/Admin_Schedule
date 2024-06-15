0009c001c531d1c7e13560a7f5bf198db599d389
“[firstname <clarklas831@gmail.com>
2024-06-14 21:33:11 -0500

    first commit

---
+++ Schedule_V3.py
@@ -0,0 +1,251 @@
+import gspread
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+import json
+import openpyxl
+import os
+import re
+
+json_file_path = 'store_runs.json'
+
+# Check if the file exists and then delete it
+if os.path.exists(json_file_path):
+    os.remove(json_file_path)
+
+
+class StoreRun:
+    def __init__(self, date, meet_time, start_time, store_note=None, employee_list=None):
+        self.date = date
+        self.meet_time = meet_time
+        self.start_time = start_time
+        self.inv_type = []
+        self.store_name = []
+        self.address = []
+        self.link = []
+        self.store_note = store_note
+        self.employee_list = {}
+
+    def add_employee(self, name, number, note):
+        self.employee_list[name] = [number, note]
+
+    def add_inv_type(self, inv_type):
+        self.inv_type.append(inv_type)
+
+    def add_store_name(self, store_name):
+        self.store_name.append(store_name)
+
+    def add_address(self, address):
+        self.address.append(address)
+
+    def add_link(self, link):
+        self.link.append(link)
+
+    def __str__(self):
+        return f"date={self.date}, meet_time={self.meet_time}, start_time={self.start_time}, inv_type={self.inv_type}, store_name={self.store_name}, address={self.address}, link={self.link}, store_note={self.store_note}, employee_list={self.employee_list})"
+
+    def to_dict(self):
+        return {
+            "date": self.date,
+            "meet_time": self.meet_time,
+            "start_time": self.start_time,
+            "inv_type": self.inv_type,
+            "store_name": self.store_name,
+            "address": self.address,
+            "link": self.link,
+            "store_note": self.store_note,
+            "employee_list": self.employee_list,
+        }
+
+
+def save_store_runs_to_json():
+    # after creating and populating the store_run object
+    json_file_path = 'store_runs.json'
+
+    try:
+        with open(json_file_path, 'r+') as file:
+            # First we load existing data into a dict.
+            file_data = json.load(file)
+            # Join new_data with file_data inside emp_details
+            file_data.append(store_run.to_dict())
+            # Sets file's current position at offset.
+            file.seek(0)
+            # convert back to json.
+            json.dump(file_data, file, indent=4)
+    except FileNotFoundError:
+        with open(json_file_path, 'w') as file:
+            # If the file doesn't exist, create a new one and write the data.
+            json.dump([store_run.to_dict()], file, indent=4)
+
+
+# Load credentials from a JSON file
+with open('config.json') as config_file:
+    credentials = json.load(config_file)
+
+# Set up the credentials for Google Sheets and Drive API
+scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/spreadsheets'
+]
+creds = Credentials.from_service_account_info(credentials, scopes=scope)
+
+# Initialize the clients for Sheets and Drive API
+client = gspread.authorize(creds)
+drive_service = build('drive', 'v3', credentials=creds)
+
+# The name of the folder you want to search for
+folder_name = 'Fake Milwaukee'
+
+# Search for the folder by name to get its ID
+query = f"mimeType='application/vnd.google-apps.folder' and name='{folder_name}'"
+folder_response = drive_service.files().list(q=query).execute()
+folders = folder_response.get('files', [])
+
+if folders:
+    folder_id = folders[0]['id']  # Taking the first folder found
+
+    # List all sheets in the specified folder using the folder ID
+    query = f"mimeType='application/vnd.google-apps.spreadsheet' and '{folder_id}' in parents"
+    file_response = drive_service.files().list(q=query).execute()
+    files = file_response.get('files', [])
+    process_columns = [2, 6, 10, 14, 18, 22, 26]
+
+    for file in files:
+        # Open the spreadsheet by ID using gspread
+        gsheet = client.open_by_key(file['id'])
+        # Print the name of the spreadsheet
+        print(f"Processing sheet: {gsheet.title}")
+
+        # Select the worksheet by title
+        worksheet = gsheet.sheet1
+
+        # Extract data from Google Sheets
+        data = worksheet.get_all_values()
+
+        # Create a new Excel workbook and sheet
+        workbook = openpyxl.Workbook()
+        sheet = workbook.active
+
+        # Insert data into the Excel sheet maintaining formatting
+        for row_index, row_data in enumerate(data, start=1):
+            for col_index, cell_data in enumerate(row_data, start=1):
+                sheet.cell(row=row_index, column=col_index).value = cell_data
+
+        workbook.save(gsheet.title + '.xlsx')
+
+        if workbook:
+            for process_col in process_columns:
+
+                # Starting from row 8 in column B
+                min_row = 8
+                current_state = 'searching'  # Initial state before setting any values
+                break_outer_loop = False  # Flag to control breaking out of the outer loop
+                header_value = sheet.cell(row=1, column=process_col - 1).value
+
+                # Iterate through the rows starting from min_row and process_col
+                for row in sheet.iter_rows(min_row=min_row, min_col=process_col, max_col=process_col):
+                    if break_outer_loop:
+                        break
+                    for cell in row:
+                        value = cell.value
+                        if value:
+                            value = value.strip()
+                        # Get the value of the cell to the left (number value)
+                        number_value = sheet.cell(
+                            row=cell.row, column=cell.column - 1).value
+                        # Get the value of the cell to the right (note value)
+                        note_value = sheet.cell(
+                            row=cell.row, column=cell.column + 1).value
+                        # Get the value of the cell two rows down (next store)
+                        next_store = sheet.cell(
+                            row=cell.row + 2, column=cell.column).value
+
+                        # Check if the current state is 'searching' and the cell contains 'meet'
+                        if value and current_state == 'searching' and 'meet' in value.lower():
+                            store_run = StoreRun(
+                                date=None, meet_time=None, start_time=None)
+                            store_run.meet_time = value
+                            current_state = 'found_meet'
+                        elif value and current_state == 'searching' and 'leave time' in value.lower():
+                            store_run = StoreRun(
+                                date=None, meet_time=None, start_time=None)
+                            store_run.meet_time = value
+                            current_state = 'found_meet'
+                        elif value and current_state == 'found_meet':
+                            store_run.start_time = value
+                            store_run.date = header_value
+                            current_state = 'found_start'
+                        # Check for anyone scheduled in the office
+                        elif value and current_state == 'searching' and 'office' in value.lower() and 'leave' not in value.lower():
+                            store_run = StoreRun(
+                                date=None, meet_time=None, start_time=None)
+                            store_run.date = header_value
+                            store_run.add_store_name(value)
+                            current_state = 'empty_value'
+                        # If the current state is 'searching' or 'found_meet', capture the start time
+                        elif value and current_state == 'searching':
+                            store_run = StoreRun(
+                                date=None, meet_time=None, start_time=None)
+                            store_run.start_time = value
+                            store_run.date = header_value
+                            current_state = 'found_start'
+                        # If the current state is 'found_start', capture the inventory type
+                        elif current_state == 'found_start':
+                            store_run.add_inv_type(value)
+                            current_state = 'found_inv_type'
+                        # If the current state is 'found_inv_type', capture the store name
+                        elif current_state == 'found_inv_type':
+                            store_run.add_store_name(value)
+                            current_state = 'found_store_name'
+                        # If the current state is 'found_store_name', capture the address
+                        elif current_state == 'found_store_name':
+                            store_run.add_address(value)
+                            current_state = 'found_store_address'
+                        # If the current state is 'found_store_address', capture the link
+                        elif current_state == 'found_store_address':
+                            store_run.add_link(value)
+                            current_state = 'found_store_link'
+                        # If the current state is 'found_store_link', handle 'TO FOLLOW' or note values
+                        elif current_state == 'found_store_link':
+                            if value == 'TO FOLLOW':
+                                current_state = 'to_follow'
+                            elif value:
+                                store_run.store_note = value
+                                current_state = 'found_store_note'
+                            else:
+                                current_state = 'empty_value'
+                        # If the current state is 'to_follow', capture another inventory type
+                        elif current_state == 'to_follow':
+                            store_run.add_inv_type(value)
+                            current_state = 'found_inv_type'
+                        # If the current state is 'found_store_note', set state to 'empty_value'
+                        elif current_state == 'found_store_note':
+                            current_state = 'empty_value'
+                        # If the current state is 'empty_value', capture employee details
+                        elif current_state == 'empty_value':
+                            if 'Office' in store_run.store_name:
+                                store_run.add_employee(
+                                    value, number_value, note_value)
+                                save_store_runs_to_json()
+                                current_state = 'searching'
+                            else:
+                                current_state = 'found_employee'
+                                store_run.add_employee(
+                                    value, number_value, note_value)
+
+                        # If the current state is 'found_employee', continue capturing employees
+                        elif current_state == "found_employee":
+                            if value:
+                                store_run.add_employee(
+                                    value, number_value, note_value)
+                            elif next_store:
+                                current_state = 'searching'
+                                save_store_runs_to_json()
+                            else:
+                                save_store_runs_to_json()
+                                break_outer_loop = True
+                                break
+
+
+else:
+    print(f"No folder found with the name {folder_name}")
---
+++ app.js
@@ -0,0 +1,233 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const loginForm = document.getElementById('loginForm');
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const fileInput = document.getElementById('fileInput');
+  const searchForm = document.getElementById('searchForm');
+
+  // Handle login form submission
+  if (loginForm) {
+    loginForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+
+      if (username === 'admin' && password === 'admin') {
+        document.getElementById('loginSection').style.display = 'none';
+        document.getElementById('uploadSection').style.display = 'block';
+      } else {
+        document.getElementById('loginError').textContent = 'Invalid username or password';
+      }
+    });
+  }
+
+  // Handle dark mode toggle
+  if (darkModeToggle) {
+    darkModeToggle.addEventListener('click', function () {
+      document.body.classList.toggle('dark-mode');
+    });
+  }
+
+  // Handle file input change
+  if (fileInput) {
+    fileInput.addEventListener('change', function (event) {
+      const file = event.target.files[0];
+      if (file && file.type === 'application/json') {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          try {
+            const json = JSON.parse(e.target.result);
+            localStorage.setItem('jsonData', JSON.stringify(json));
+            document.getElementById('searchSection').style.display = 'block';
+          } catch (error) {
+            console.error('Error parsing JSON:', error);
+            document.getElementById('jsonOutput').textContent = 'Error parsing JSON file.';
+          }
+        };
+        reader.readAsText(file);
+      } else {
+        document.getElementById('jsonOutput').textContent = 'Please upload a valid JSON file.';
+      }
+    });
+  }
+
+  // Handle search form submission
+  if (searchForm) {
+    searchForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+      const employeeName = document.getElementById('employeeName').value.trim();
+      if (employeeName) {
+        const jsonData = JSON.parse(localStorage.getItem('jsonData'));
+        const results = searchEmployeeRuns(jsonData, employeeName);
+        displaySearchResults(results, employeeName);
+        employeeNameHeader.textContent = `Results for: ${employeeName.toUpperCase()}`; // Set the h2 text
+        document.getElementById('employeeName').value = ''; // Clear the textbox
+      }
+    });
+  }
+});
+
+// Search for employee runs
+function searchEmployeeRuns(json, employeeName) {
+  return json.filter((run) => {
+    return Object.keys(run.employee_list).some((employee) => employee.toLowerCase().includes(employeeName.toLowerCase()));
+  });
+}
+
+// Display search results
+function displaySearchResults(results, employeeName) {
+  const resultsContainer = document.getElementById('resultsContainer');
+  resultsContainer.innerHTML = '';
+
+  if (results.length === 0) {
+    resultsContainer.textContent = 'No runs found for the specified employee.';
+    return;
+  }
+
+  results.forEach((run) => {
+    const runElement = document.createElement('div');
+    runElement.classList.add('card');
+
+    const date = document.createElement('h3');
+    date.textContent = run.date;
+    runElement.appendChild(date);
+
+    if (run.meet_time) {
+      const meetTime = document.createElement('p');
+      meetTime.innerHTML = `<strong>Meet Time:</strong> ${run.meet_time}`;
+      runElement.appendChild(meetTime);
+    }
+
+    if (run.start_time) {
+      const startTime = document.createElement('p');
+      startTime.innerHTML = `<strong>Start Time:</strong> ${run.start_time}`;
+      runElement.appendChild(startTime);
+    }
+
+    if (run.store_note) {
+      const storeNote = document.createElement('p');
+      storeNote.innerHTML = `<strong>Note:</strong> ${run.store_note}`;
+      runElement.appendChild(storeNote);
+    }
+
+    let supervisor = '';
+    let drivers = [];
+    Object.keys(run.employee_list).forEach((employee) => {
+      const [number, note] = run.employee_list[employee];
+      if (number === '1)') {
+        supervisor = employee;
+      }
+      if (note.toLowerCase().includes('driver')) {
+        drivers.push(employee);
+      }
+    });
+
+    if (supervisor) {
+      const supervisorElement = document.createElement('p');
+      supervisorElement.innerHTML = `<strong>Supervisor:</strong> ${supervisor}`;
+      runElement.appendChild(supervisorElement);
+    }
+
+    if (run.meet_time && drivers.length > 0) {
+      const driversElement = document.createElement('p');
+      driversElement.innerHTML = `<strong>Drivers:</strong> ${drivers.join(' | ')}`;
+      runElement.appendChild(driversElement);
+    }
+
+    const storeCardContainer = document.createElement('div');
+    storeCardContainer.classList.add('store-card-container');
+    if (run.store_name.length > 1) {
+      storeCardContainer.classList.add('hidden');
+    }
+
+    run.store_name.forEach((store, index) => {
+      const storeCard = document.createElement('div');
+      storeCard.classList.add('store-card');
+
+      const storeName = document.createElement('p');
+      storeName.innerHTML = `<strong>${store}</strong>`;
+      storeCard.appendChild(storeName);
+
+      if (run.inv_type[index] !== undefined) {
+        const invType = document.createElement('p');
+        invType.innerHTML = `${run.inv_type[index]}`;
+        storeCard.appendChild(invType);
+      }
+
+      const link = document.createElement('a');
+      link.href = run.link[index];
+      link.textContent = run.address[index];
+      storeCard.appendChild(link);
+
+      storeCardContainer.appendChild(storeCard);
+    });
+
+    if (run.store_name.length > 1) {
+      const toggleStoreButton = document.createElement('button');
+      toggleStoreButton.textContent = 'Toggle Stores';
+      toggleStoreButton.classList.add('show-all');
+      toggleStoreButton.addEventListener('click', () => {
+        storeCardContainer.classList.toggle('hidden');
+      });
+      runElement.appendChild(toggleStoreButton);
+    }
+
+    runElement.appendChild(storeCardContainer);
+
+    const employeeList = document.createElement('ul');
+    employeeList.classList.add('employee-list', 'hidden');
+    const isSpecialEmployee = Object.keys(run.employee_list).some((employee) => {
+      if (employee.toLowerCase().includes(employeeName.toLowerCase())) {
+        const [number, note] = run.employee_list[employee];
+        return number === '1)' || (note.toLowerCase().includes('driver') && !note.toLowerCase().includes('@ store'));
+      }
+      return false;
+    });
+
+    if (isSpecialEmployee) {
+      Object.keys(run.employee_list).forEach((employee) => {
+        const [number, note] = run.employee_list[employee];
+        if (employee.toLowerCase() !== employeeName.toLowerCase() && !note.toLowerCase().includes('@ store')) {
+          const listItem = document.createElement('li');
+          listItem.innerHTML = `<strong>${employee}</strong>`; // Only display the employee name
+          employeeList.appendChild(listItem);
+        }
+      });
+    }
+
+    let isSupervisor = false;
+    Object.keys(run.employee_list).forEach((employee) => {
+      const [number] = run.employee_list[employee];
+      if (employee.toLowerCase().includes(employeeName.toLowerCase()) && number === '1)') {
+        isSupervisor = true;
+      }
+    });
+
+    if (isSupervisor) {
+      employeeList.innerHTML = '';
+      Object.keys(run.employee_list).forEach((employee) => {
+        const [number, note] = run.employee_list[employee];
+        if (employee.toLowerCase() !== employeeName.toLowerCase()) {
+          const listItem = document.createElement('li');
+          if (note != '') {
+            listItem.innerHTML = `<strong>${employee}</strong> - ${note}`; // Display the employee name and note separated by hyphen
+          } else {
+            listItem.innerHTML = `<strong>${employee}</strong>`;
+          }
+          employeeList.appendChild(listItem);
+        }
+      });
+    }
+
+    if (employeeList.childElementCount > 0) {
+      const toggleEmployeeButton = document.createElement('button');
+      toggleEmployeeButton.textContent = 'Toggle Employees';
+      toggleEmployeeButton.addEventListener('click', () => {
+        employeeList.classList.toggle('hidden');
+      });
+      runElement.appendChild(toggleEmployeeButton);
+    }
+
+    runElement.appendChild(employeeList);
+    resultsContainer.appendChild(runElement);
+  });
+}
